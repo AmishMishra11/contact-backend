a79a95878eac8aa6468f765cc275c7ef4f3da54b
AmishMishra11 <amishmishra11@gmail.com>
2023-09-16 17:47:09 +0530

    fix: added cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
 import { connectToMongoose } from "./db/db.connect";
+import cors from "cors";
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(urlencoded({ extended: true }));
+app.use(cors());
 
 connectToMongoose();
 
